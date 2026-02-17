83c810c450def1ca18cf3e8772f5abccc5e7049a
Berat Erol <beratlore00@gmail.com>
2026-02-17 16:15:14 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h3>LAB-1</h3>
+      <p><strong>Ad Soyad:</strong> [Berat Erol]</p>
+      <p><strong>Öğrenci No:</strong> [235541010]</p>
+    </div>
+  )
+}
+
+export default App
